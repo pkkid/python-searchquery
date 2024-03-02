ae7894a5da7d181d19ecd773db81568fbc58aeca
Michael Shepanski <mshepanski@nasuni.com>
2024-03-02 18:56:56 -0500

    first.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# django-search
+# django-querysearch
+Simple query search module for Django
+
+## Installation
+```
+pip install git+https://github.com/pkkid/django-searchquery.git
+```
+
+## Usage
+Once you define the searchable columns, the user will be able to search the 
+result set using a friendly syntax. The module will parse the search string and
+return a Djazngo Query object.
+
+```python
+from django_searchquery.search import FIELDTYPES, SearchField
+from myapp.applications.models import Application
+
+SEARCH_FIELDS = [
+    SearchField('name', FIELDTYPES.STR, 'name'),
+    SearchField('age', FIELDTYPES.NUM, 'age'),
+    SearchField('date', FIELDTYPES.NUM, 'date'),
+]
+
+basequeryset = Application.objects.all()
+search = Search(basequeryset, SEARCH_FIELDS, 'age>30 date>"2 weeks ago" name=Michael')
+results = search.queryset()
+```
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from pkg_resources import parse_requirements
+from setuptools import setup
+
+readme = open('README.md', 'r').read()
+with open('requirements.txt') as handle:
+    requirements = [str(req) for req in parse_requirements(handle)]
+
+setup(
+    name='django_searchquery',
+    version='0.9',
+    description='Simple query search module for Django',
+    author='Michael Shepanski',
+    author_email='michael.shepanski@gmail.com',
+    url='https://github.com/pkkid/django-searchquery',
+    packages=['django_querysearch'],
+    install_requires=requirements,
+    python_requires='>=3.7',
+    long_description=readme,
+    keywords=['django', 'search', 'query', 'orm'],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+    ]
+)
