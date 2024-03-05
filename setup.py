523cd71646c92da1eb2d6f06a5bdea28e268767f
Michael Shepanski <mshepanski@nasuni.com>
2024-03-04 21:35:03 -0500

    New version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as handle:
 
 setup(
     name='django_searchquery',
-    version='1.0',
+    version='1.0.1',
     description='Simple query search module for Django',
     author='Michael Shepanski',
     author_email='michael.shepanski@gmail.com',
